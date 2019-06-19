bf3dbfc448ab21b2b3bca08c0346b13c5d7dd439
fangle <fank243@163.com>
2019-06-19 10:31:30 +0800

    红重新打包

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@
 # 需要修改 -DartifactId、-Dfile参数
 
 mvn deploy:deploy-file \
---settings="${MVN_HOME}/conf"/settings.xml \
+--settings="${MAVEN_HOME}/conf"/settings.xml \
 -DgroupId=com.qhwr.third \
 -DartifactId=qhwr-fuiou \
 -Dversion=1.0.0 \
 -Dpackaging=jar \
--Dfile=C:/Users/fank2/Desktop/qhwr-fuiou-1.0.0.jar \
+-Dfile=C:/Users/dell/Desktop/qhwr-fuiou-1.0.0.jar \
 -DrepositoryId=releases \
--Durl=file:E:/workspaces/idea/qhwrong/mvnrepository
+-Durl=file:E:/workspaces/idea/qhwrong/qhwr-github/mvnrepository
