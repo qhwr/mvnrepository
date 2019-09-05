410d0c8053d0fc329e3dfacc6256032a0fc887d8
laher <weisheixiaoxin@qq.com>
2019-09-05 17:49:12 +0800

    [新增]   百荣sdk包

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,10 @@
 mvn deploy:deploy-file \
 --settings="${MAVEN_HOME}/conf"/settings.xml \
 -DgroupId=com.qhwr.third \
--DartifactId=qhwr-fuiou \
+-DartifactId=qhwr-bairong \
 -Dversion=1.0.0 \
 -Dpackaging=jar \
--Dfile=C:/Users/dell/Desktop/qhwr-fuiou-1.0.0.jar \
+-Dfile=D:/laher/workspace/intellij/lib/bairong-sdk-3.6.1.jar \
 -DrepositoryId=releases \
--Durl=file:E:/workspaces/idea/qhwrong/qhwr-github/mvnrepository
+-Durl=file:D:/laher/workspace/intellij/mvnrepository
+# -Durl=file:E:/workspaces/idea/qhwrong/qhwr-github/mvnrepository
